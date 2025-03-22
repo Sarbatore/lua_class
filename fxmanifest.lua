0cac1aec499933197ea2dbd7a66a0fac62a1202c
Sarbatore <66779630+Sarbatore@users.noreply.github.com>
2025-03-22 20:14:00 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,10 @@ game "rdr3"
 fx_version "adamant"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 "yes"
+
 author "Sarbatore"
+description "Developer library to create classes in Lua"
 
 shared_scripts {
-  
+    "import.lua",
 }
